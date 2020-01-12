121080f3d05e94e7a366a0c96bfcb159fcd3a54e
brian d foy <brian.d.foy@gmail.com>
2020-01-11 20:26:01 -0500

    Simon Phipps on open source

---
+++ _posts/2008-08-30-simon-phipps-on-open-source.pl
@@ -0,0 +1,21 @@
+---
+layout: post
+title: Simon Phipps on Floss Weekly
+tags: open-source
+---
+
+For [FLOSS Weekly 39](https://twit.tv/shows/floss-weekly/episodes/39), Leo Laporte and Randal Schwartz interviewed Simon Phipps, the Chief Open Source Officer at Sun. It's not the normal open source religion, and a much better view than I've heard for the "Open Source" people.
+
+The entire hour long interview is excellent, but this was my favorite part:
+
+
+**Simon Phipps (@0:14:08)**: What characterizes open source is, open source is the syncronization of the self interest of many parties. And to create an environment where people are willing to synchronize their self interest and collaborate over code, there has to be transparency. On the other hand, in open source, you know, Randal, I don't care what your motivations are for being involved in Perl. They're of no relevance to my life because our relationship around Perl depends on code and the code and the community are transparent, but your motivations for participating in it are opaque. It's up to me. They're private to me.
+
+**Leo Laporte**: They're also irrelevant because of transparency, the codes speaks for itself.
+
+**Simon**: Absolutely. So I'm able to maintain my privacy around my motivations and degree of my involvement and how I'm funding it. I maintain responsibility for that part that is private as well. On the other hand, I'm able to work in an environment of transparency where all the code is known, all its origins are known, all its defects are potentially known, and that combination of transparency with privacy is, in my opinion, what characterizes open source. Trying to define open source in terms of licenses in kinda outmoded in my view. Open source is about transparency at the community level but also about privacy in terms of my motivations.
+
+
+Simon also said quoted:
+
+> Whenever you create a system, you create the game that plays it.
