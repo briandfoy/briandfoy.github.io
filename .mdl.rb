5f44cb89a91e7e1ca0a1a4452ecfe04facae12da
brian d foy <briandfoy@pobox.com>
2025-08-10 12:18:34 -0400

    get mdl style from Ruby file

---
+++ .mdl.rb
@@ -0,0 +1,9 @@
+all
+exclude_rule 'MD002' # first header should be a top level header
+exclude_rule 'MD012' # multiple consecutive blank lines
+exclude_rule 'MD013' # line length
+exclude_rule 'MD033' # no inline HTML
+exclude_rule 'MD036' # emphasis used instead of a header
+exclude_rule 'MD041' # first line should be a top level header
+exclude_rule 'MD046' # code block style
+rule 'MD026', :punctuation => ".,;:"
